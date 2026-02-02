dfa9b56c963b803bd1d59caa557245868aaf9283
Dean Howe <hideanhowe@gmail.com>
2026-02-02 13:36:14 +0000

    testing

--- index.js
+++ index.js
@@ -4,3 +4,26 @@
 // All consonants should be converted to uppercase.
 // All vowels (a, e, i, o, u in any case) should be converted to lowercase.
 // All hyphens (-) should be converted to underscores (_).
+
+function toConsonantCase(str) {
+  console.log(str);
+  // all consonants to uppercase
+  const up = str.toUpperCase();
+  console.log(up);
+  // all vowels to lowercase
+  const regex = /aeiou/gi;
+  // put into array
+  const arr = [];
+  for (let i = 0; i < up.length; i++) {
+    arr.push(up[i]);
+  }
+  console.log(arr);
+  const lower = arr.map((letter) => {
+    return regex.test(letter).toLowerCase();
+  });
+
+  console.log(lower);
+
+  // all hyphens to underscores
+  return str;
+}
